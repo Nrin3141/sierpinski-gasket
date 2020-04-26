22b398ca10eadb71714d51e101d6fb085fa0596b
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-04-26 23:20:15 +0200

    Add background color changer

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ class Configuration {
     this.maxIterations = 8;
     this.color = "#ffe100";
     this.fillColor = "#000000";
+    this.background = "#444444";
     this.name = "img_name";
     this.save = () => saveCanvas(canvas, this.name, "jpg");
   }
@@ -11,20 +12,15 @@ let config;
 let length;
 let canvas;
 function setup() {
-  canvas = createCanvas(window.innerWidth, window.innerHeight);
-  if (window.innerWidth > window.innerHeight) {
-    length = window.innerHeight * 0.8;
-  } else {
-    length = window.innerWidth * 0.8;
-  }
   config = new Configuration();
   drawSierpinskiGasket();
-
+  window.addEventListener("resize", drawSierpinskiGasket);
   const gui = new dat.GUI();
   const o = gui.addFolder("Options");
   const iterationController = o.add(config, "maxIterations", 1, 10).step(1);
   const colorController = o.addColor(config, "color");
   const fillColorController = o.addColor(config, "fillColor");
+  const backgroundController = o.addColor(config, "background");
 
   const saving = gui.addFolder("Save File");
   saving.add(config, "name");
@@ -33,10 +29,17 @@ function setup() {
   iterationController.onChange(drawSierpinskiGasket);
   colorController.onChange(drawSierpinskiGasket);
   fillColorController.onChange(drawSierpinskiGasket);
+  backgroundController.onChange(drawSierpinskiGasket);
 }
 
 function drawSierpinskiGasket() {
-  background(51);
+  canvas = createCanvas(window.innerWidth, window.innerHeight);
+  if (window.innerWidth > window.innerHeight) {
+    length = window.innerHeight * 0.8;
+  } else {
+    length = window.innerWidth * 0.8;
+  }
+  background(config.background);
   angleMode(DEGREES);
   resetMatrix();
   translate(width / 2, (height - (length * sqrt(3)) / 2) / 2);
